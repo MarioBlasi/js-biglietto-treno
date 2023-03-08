d27089603d444d50072b5ecb4a6550041ca9fcf4
MarioBlasi <mblasi509@gmail.com>
2023-03-08 15:22:09 +0000

    prezzo passeggero

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,3 @@
-alert("welcome");
 /*chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 
@@ -11,8 +10,42 @@ per indicare centesimi sul prezzo).*/
 /*------------------*/
 
 //chiedere al passeggero il numero di km che vuole percorrere
+//prompt and // const
 //chiedere l'etá del passeggero
+//prompt and // const
 //calcolare il prezzo totale del viaggio (0.21 € al km)
+// 0.21£ x km da percorrere
+
 //applicare scoto 20% <18 anni
+// Verifico SE il passeggero ha meno di 18 anni applico lo sconto
 //applicare sconto 40% >65 anni
+// Verifico SE il passeggero ha piú di 65 anni applico lo sconto
+// ALTRIMENTI NON applico Nessuno sconto
+
 // prezzo finale riportato fuori nel browser
+
+const userKm = prompt("quanti km vuoi percorrere?");
+console.log(userKm);
+/*---------chilometri da fare----*/
+
+const prezzoKm = 0.21;
+console.log(prezzoKm);
+
+const spesa = "userKm" * "prezzoKm";
+console.log(spesa);
+
+const userAge = prompt("quanti anni hai?");
+console.log(userAge);
+/*--------etá del passeggero----*/
+
+/*
+const user = passeggero;
+console.log(user)
+
+if (passeggero < 18) {
+    console.log("sconto del 20%");
+  } else if (passeggero > 18  && passeggero < 65) {
+    console.log("niente sconto");
+  } else (passeggero > 65) {
+    console.log("sconto 40%");
+  }*/
